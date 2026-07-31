95348dc98e3df400588355308c65f70c7d57580b
Aabu Yousuf Raj <aabuyousufraj@gmail.com>
2026-07-31 01:14:57 -0700

    Update leadership.ts

--- src/data/leadership.ts
+++ src/data/leadership.ts
@@ -11,10 +11,27 @@ export const leadershipData: Leadership[] = [
     date: "October 2024 - January 2026",
     title: "Vice President, BRAC University Marketing Association (BUMA)",
     description: 
-      "As Vice President of the BRAC University Marketing Association (BUMA), I led the planning and execution of events, sponsorships, campaigns, and collaborations that strengthened the association’s presence on campus. I organized competitions such as programming contests, esports competitions, bootcamps and CTF events, along with high-energy extracurricular programs like cultural shows, dance mobs, treasure hunts, indoor and outdoor games, and special-day fests. I also collaborated with the Office of Career Services and Alumni Relations of BRACU to host career-focused seminars and sessions, while coordinating teams, logistics, and partnerships to deliver smooth, impactful experiences.",
+      "As Vice President of the BRAC University Marketing Association (BUMA), I led teams to execute high-impact campaigns, sponsorships, and diverse university-wide events. My event portfolio ranged from technical CTFs and programming contests  to esports tournaments and cultural fests. By streamlining logistics and partnering with BRACU Career Services for professional seminars, I successfully strengthened the association’s campus and industry presence.",
     imageUrl: "/assets/images/leadership/club bootcamp.png",
     linkUrl: "",
   },
+  {
+    date: "September 2022 - September 2023",
+    title: "Senior Executive (R&D), BRAC University Entrepreneurship Development Forum (BUEDF)",
+    description: 
+      "Supported the Research & Development department by strategizing and executing campus-wide entrepreneurial initiatives, startup workshops, and innovative events.",
+    imageUrl: "",
+    linkUrl: "",
+  },
+  {
+    date: "October 2023 - May 2024",
+    title: "General Member, BRAC University Football Club",
+    description: 
+      "Actively participated in club training sessions, team-building activities, and competitive university football events to foster athletic development.",
+    imageUrl: "",
+    linkUrl: "",
+  }
+];
   // Add more leadership entries as needed
 ];
   
