693609cde5674e9c37cee4d81604468c8ee82be5
Aabu Yousuf Raj <aabuyousufraj@gmail.com>
2026-07-31 02:18:30 -0700

    Update experience.ts

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -12,11 +12,26 @@ export interface Experience {
 export const experienceData: Experience[] = [
   {
     date: "June 2026 - Present",
+    title: "Research Assistant",
+    company: "United International University (UIU)",
+    location: "Dhaka, Bangladesh",
+    description:
+      "At Computing for Sustainability and Social Good (C2SG) research group, under the supervision of Dr. Jannatun Noor, I conduct core research focused on computer vision and deep learning applications for AgTech. My primary role involves contributing to the design and implementation of robust machine learning pipelines aimed at advancing precision agriculture, developing low-cost intelligence, and creating applied AI solutions.",
+  },
+  {
+    date: "June 2026 - Present",
     title: "AI Engineer Trainee",
     company: "Chowa Giken Corporation",
     location: "Dhaka, Bangladesh",
     description:
-      "Selected as one of 10 participants from BRAC University from a pool of 1,000+ applicants for the NIPPON AI Dojo Program, an applied AI training initiative organized by Chowa Giken Corporation and AI Samurai Co. Ltd., with support from METI, Japan. Participating in industry-oriented on-the-job training focused on real-world AI development, AI engineering workflows, and Japanese engineering practices.",
+      "Selected as one of 10 participants from BRAC University from a pool of 1,000+ applicants for the NIPPON AI Dojo Program, an applied AI training initiative organized by Chowa Giken Corporation and AI Samurai Co. Ltd., with support from METI, Japan. Participating in industry-oriented on-the-job training focused on real-world AI development, and AI engineering workflows."
+  {
+    date: "June 2026 - Present",
+    title: "Machine Learning Intern",
+    company: "FlyRank AI",
+    location: "Dhaka, Bangladesh (Remote)",
+    description:
+      "Building an end-to-end machine learning project centered around an interactive user interface. Responsibilities include developing and integrating machine learning models with front-end components to create an adaptive, interactive learning experience. Focused on optimizing both model performance and system interactivity for real-world application.",
   },
   {
     date: "February 2025 - May 2025",
